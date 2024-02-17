911d4b2313f7751bd5c38f4de0f6dfef827f0ef9
Manraaj Nijjar <manraajnijjar@gmail.com>
2024-02-17 00:22:31 -0500

    InitialCommits

---
+++ ScreamJamGodot/assets/phone/phone_screen.gd
@@ -0,0 +1,95 @@
+extends Node3D
+
+@onready var display = $MeshInstance3D
+@onready var viewport : SubViewport = $SubViewport
+@onready var area = $Area3D
+
+var mesh_size = Vector2();
+
+var mouse_entered = false;
+var mouse_held = false;
+var mouse_inside = false;
+
+var last_mouse_pos_3D = null;
+var last_mouse_pos_2D = null;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	area.mouse_entered.connect(func(): mouse_entered = true)
+	viewport.set_process_input(true);
+	pass # Replace with function body.
+
+func _unhandled_input(event):
+	var is_mouse_event = false;
+	if event is InputEventMouseMotion || event is InputEventMouseButton:
+		is_mouse_event = true;
+	
+	if mouse_entered && (is_mouse_event || mouse_held):
+		handle_mouse(event);
+	elif !is_mouse_event:
+		viewport.push_input(event);
+
+func handle_mouse(event):
+	mesh_size = display.mesh.size;
+
+	if event is InputEventMouseButton || event is InputEventScreenTouch:
+		
+		mouse_held = event.pressed;
+	
+	var mouse_pos3d = find_mouse(event.global_position);
+	mouse_inside = mouse_pos3d != null;
+
+	if mouse_inside:
+		mouse_pos3d = area.global_transform.affine_inverse() * mouse_pos3d;
+		last_mouse_pos_3D = mouse_pos3d;
+	else:
+		mouse_pos3d = last_mouse_pos_3D;
+		if mouse_pos3d == null:
+			mouse_pos3d = Vector3.ZERO;
+
+	var mouse_pos2d = Vector2(mouse_pos3d.x, -mouse_pos3d.y);
+
+	#Converts from -mesh_size/2 to mesh_size/2
+	mouse_pos2d.x += mesh_size.x / 2;
+	mouse_pos2d.y += mesh_size.y / 2;
+
+	#Converts to 0 to 1
+	mouse_pos2d.x = mouse_pos2d.x / mesh_size.x;
+	mouse_pos2d.y = mouse_pos2d.y / mesh_size.y;
+	
+	#Converts to viewport range
+	mouse_pos2d.x = mouse_pos2d.x * viewport.size.x;
+	mouse_pos2d.y = mouse_pos2d.y * viewport.size.y;
+
+	event.position = mouse_pos2d;
+	event.global_position = mouse_pos2d;
+
+	if event is InputEventMouseMotion:
+		if last_mouse_pos_2D == null:
+			event.relative = Vector2.ZERO;
+		else:
+			event.relative = mouse_pos2d - last_mouse_pos_2D;
+		
+	last_mouse_pos_2D = mouse_pos2d;
+	viewport.push_input(event);
+
+func find_mouse(pos: Vector2):
+	var camera = get_viewport().get_camera_3d();
+
+	var dss:PhysicsDirectSpaceState3D = get_world_3d().direct_space_state;
+
+	var rayparam = PhysicsRayQueryParameters3D.new();
+	rayparam.from = camera.project_ray_origin(pos);
+
+	var distance = 5;
+
+	rayparam.to = rayparam.from + camera.project_ray_normal(pos) * distance;
+	rayparam.collide_with_bodies = false;
+	rayparam.collide_with_areas = true;
+
+	var result = dss.intersect_ray(rayparam);
+	if result.size() > 0:
+		print(result.position)
+		return result.position;
+	else:
+		return null;
---
+++ ScreamJamGodot/assets/player/player.gd
@@ -0,0 +1,55 @@
+extends CharacterBody3D
+
+
+const SPEED = 3.0
+const mouseSensitivity = 0.2;
+
+@onready var camera = $head/Camera3D
+@onready var head = $head
+
+@export var canMove = true;
+@export var lockMouseOnStart = true;
+
+@export var xClamp = 90;
+@export var clampY = false;
+@export var yClamp = 90;
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	if(lockMouseOnStart):
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * mouseSensitivity))
+		head.rotate_x(deg_to_rad(-event.relative.y * mouseSensitivity))
+		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-xClamp), deg_to_rad(xClamp))
+		if(clampY):
+			rotation.y = clamp(rotation.y, deg_to_rad(-yClamp), deg_to_rad(yClamp))	
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	if !canMove:
+		return;
+
+	if Input.is_action_pressed("viewPhone"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE);
+	elif lockMouseOnStart:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED);
+		
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
