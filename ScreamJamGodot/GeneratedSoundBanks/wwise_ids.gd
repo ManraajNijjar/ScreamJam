1517f7c26a9f65082d343cdf872a7588c0d2efd2
ashycodes <64503359+ashycodes@users.noreply.github.com>
2024-02-18 22:33:31 -0600

    Hooked up Wwise to Godot project

---
+++ ScreamJamGodot/GeneratedSoundBanks/wwise_ids.gd
@@ -0,0 +1,62 @@
+class_name AK
+
+class EVENTS:
+
+	const TEST = 3157003241
+
+	const _dict = {
+		"TEST": TEST
+	}
+
+class STATES:
+
+	const _dict = {}
+
+class SWITCHES:
+
+	const _dict = {}
+
+class GAME_PARAMETERS:
+
+	const _dict = {}
+
+class TRIGGERS:
+
+	const _dict = {}
+
+class BANKS:
+
+	const INIT = 1355168291
+	const MAINSOUNDBANK = 534561221
+
+	const _dict = {
+		"Init": INIT,
+		"MainSoundbank": MAINSOUNDBANK
+	}
+
+class BUSSES:
+
+	const MASTER_AUDIO_BUS = 3803692087
+
+	const _dict = {
+		"Master Audio Bus": MASTER_AUDIO_BUS
+	}
+
+class AUX_BUSSES:
+
+	const _dict = {}
+
+class AUDIO_DEVICES:
+
+	const SYSTEM = 3859886410
+	const NO_OUTPUT = 2317455096
+
+	const _dict = {
+		"System": SYSTEM,
+		"No_Output": NO_OUTPUT
+	}
+
+class EXTERNAL_SOURCES:
+
+	const _dict = {}
+
