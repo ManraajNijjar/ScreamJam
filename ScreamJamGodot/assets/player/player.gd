dc256d83b6888186de8ee9542b9ffb799300d1f1
Manraaj Nijjar <manraajnijjar@gmail.com>
2024-02-18 22:26:30 -0500

    Detailed out Subway Scene

--- ScreamJamGodot/assets/player/player.gd
+++ ScreamJamGodot/assets/player/player.gd
@@ -6,6 +6,8 @@ const mouseSensitivity = 0.2;
 
 @onready var camera = $head/Camera3D
 @onready var head = $head
+@onready var animationPlayer = $AnimationPlayer
+var phoneInPlace = true;
 
 @export var canMove = true;
 @export var lockMouseOnStart = true;
@@ -22,7 +24,7 @@ func _ready():
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(event):
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion && !Input.is_action_pressed("viewPhone"):
 		rotate_y(deg_to_rad(-event.relative.x * mouseSensitivity))
 		head.rotate_x(deg_to_rad(-event.relative.y * mouseSensitivity))
 		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-xClamp), deg_to_rad(xClamp))
@@ -35,14 +37,22 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	if !canMove:
-		return;
-
+	
 	if Input.is_action_pressed("viewPhone"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE);
+		if(phoneInPlace):
+			phoneInPlace = false;
+			animationPlayer.play("MovePhoneToFace");
 	elif lockMouseOnStart:
+		if(!phoneInPlace):
+			phoneInPlace = true;
+			animationPlayer.play("MovePhoneBack");
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED);
-		
+	
+	if !canMove:
+		return;
+	
+
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
