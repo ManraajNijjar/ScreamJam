af66ee6f73d305141600fedc141feb6065817d31
beep salt <beepsalt@gmail.com>
2024-02-21 12:41:31 -0600

    added custom balloon

---
+++ ScreamJamGodot/narrative/balloon_test_scene.gd
@@ -0,0 +1,9 @@
+extends BaseDialogueTestScene
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var balloon = load("res://narrative/balloon.tscn").instantiate()
+	get_tree().current_scene.add_child(balloon)
+	balloon.start(resource, title)
+
